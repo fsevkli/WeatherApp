65c36a171bcd57f6a2c576ac2ad3d909526499e4
sevkliyf <sevkliyf@miamioh.edu>
2023-09-07 15:47:27 -0400

    Minor update

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
+from urllib.request import urlopen
+import json
 
 app = Flask(__name__)
 
+# How to get weather data from location in API
+# response = urlopen(f"https://api.weatherapi.com/v1/current.json?key=5dea31b4204948a681b182600230709&q={loc}")
+# data_json = json.loads(response.read())
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", data="")
 
-@app.route('/store')
-def store():
-    return "i sell kebab"
+@app.route('/location/<loc>')
+def test(loc: str = "Paris"):
+    response = urlopen(f"https://api.weatherapi.com/v1/current.json?key=5dea31b4204948a681b182600230709&q={loc}")
+    data_json = json.loads(response.read())
+
+    return render_template("index.html", data=data_json)
 
 if __name__ == '__main__':
     # Port is set to 8080, change to anything you want
