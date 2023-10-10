e067b0fa3096a2174e64ea94454982a1e4395970
Vexl1 <51213834+Vexl1@users.noreply.github.com>
2023-10-10 13:29:06 -0400

    Updated app.py to work with ajax requests added UI files and javascript.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 from urllib.request import urlopen
 import json
 import geocoder
@@ -8,51 +8,71 @@ app = Flask(__name__)
 
 API_KEY = '5dea31b4204948a681b182600230709'
 
-# How to get weather data from location in API
-# response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={loc}")
-# data_json = json.loads(response.read())
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/location/<loc>')
-def location(loc: str):
-    if loc == "current":
-        latlng = get_location()
-        response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={latlng[0]},{latlng[1]}&days=14")
-    else:
-        response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={loc}&days=14")
-
-    data = json.loads(response.read())
-    
-    # Saves data to json
-    with open(f'loc_json/loc_{loc}.json', 'w') as f:
-        json.dump(data, f, indent=4)
-
-    return render_template("location.html", data=data, 
-                           location=data['location'], current=data['current'], condition=data['current']['condition'],
-                           forecast=data['forecast']['forecastday'])
-
-@app.route('/search')
-def search():
-    query = request.args.get('query', 'Paris')
-    query = query.split(" ")[0]  # Do this because API has issues with spaces between words
-    response = urlopen(f"https://api.weatherapi.com/v1/search.json?key={API_KEY}&q={query}")
-    data = json.loads(response.read())
-
-    # Saves data to json
-    with open(f'loc_json/search_{query}.json', 'w') as f:
-        json.dump(data, f, indent=4)
-
-    return render_template("search.html", data=data)
-
+################################
+# This function Get the 
+# location of the user 
+# via ip address
+################################
 def get_location():
     g = geocoder.ip('me')
     return g.latlng
 
+################################
+# This function makes a api call 
+# to weatherapi to get the weekly 
+# forecast
+################################
+def location(loc: str):
+    print("testing")
+    if loc == "current":
+        latlng = get_location()
+       
+        response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={latlng[0]},{latlng[1]}&days=14")
+    else:
+       # get the input from text box html
+       input = request.args.get('inputValue')
+       response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={input}&days=14")
+
+    data = json.loads(response.read())
+    
+    #Convert data to json to use in main.js
+    return jsonify(data=data, 
+                           location=data['location'], current=data['current'], condition=data['current']['condition'],
+                           forecast=data['forecast']['forecastday'])
+
+def search():
+    # Do this because the API has issues with spaces between words
+    query = request.args.get('inputValue')
+    query = query.split(" ")[0]  # Do this because API has issues with spaces between words
+    response = urlopen(f"https://api.weatherapi.com/v1/search.json?key={API_KEY}&q={query}")
+    data = json.loads(response.read())
+
+    # Save data to JSON file (optional)
+   
+    return data
+
+################################
+# This functions renders the 
+# index webpage 
+################################
+@app.route('/')
+def index():
+
+   if request.is_json:
+        json_data = search()
+        json_data = location(str)
+        
+        print(json_data)
+       
+        return json_data
+   return render_template("index.html")
+
+################################
+# This Function Runs the server 
+# on a local machine 
+################################
 if __name__ == '__main__':
     # Port is set to 8080, change to anything you want
-    #   Remember, has to be 1024 or above
+    # Remember, it has to be 1024 or above
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
---
+++ static/Javascript/main.js
@@ -0,0 +1,68 @@
+// Wait until html is done rendering 
+$(document).ready(function() {
+    
+$("#Location").on('keyup', function (event) {
+    if (event.keyCode === 13) {
+        console.log("Enter key pressed!!!!!");
+        var inputValue = $(this).val();
+        console.log(inputValue);
+        $.ajax({
+            url:'',
+            type:'get',
+            contentType:'application/json',
+            data: {
+               inputValue: inputValue 
+            }, 
+            success: function (response) {
+                console.log(response)
+              
+        const cardsContainer = $('#Cards');
+        cardsContainer.empty(); // Clear existing card data
+        const forecastWeek  = response
+
+        // Loop through the forecast data and create cards
+        for (const forecast of forecastWeek.forecast) {
+        //date string in "YYYY-MM-DD" format
+        var dateString = forecast.date;
+
+        // Parse the date string into a Date object
+        var date = new Date(dateString);
+
+        // Array of day names
+        var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+        var dayOfWeek = date.getDay();
+
+        // Get the name of the day
+        var dayName = dayNames[dayOfWeek];
+        console.log(forecast)
+        // Append a new card to div Cards 
+           const cardHtml = `
+            <div class="col-md-2">
+              <div id="CardBlock">
+                <div class="card">
+                  <img class="card-img-top" src="${forecast.day.condition.icon}" alt="Card image">
+                  <div class="card-img-overlay" style="text-align: center;">
+                    <h4 class="card-title"><b>${dayName}</b></h4>
+                    <p class="card-text"><h3>${forecast.day.condition.text}</h3></p>
+                    <img src="static/Images/thermometer.png" alt="Card image" width="20" height="20">
+                    ${forecast.day.avgtemp_f}°F
+                  </div>
+                </div>
+              </div>
+            </div>
+          `;
+
+          cardsContainer.append(cardHtml);
+        }
+      },
+      error: function(error) {
+        console.error('Error:', error);
+      }    
+        })
+     }
+ })
+
+})
+
