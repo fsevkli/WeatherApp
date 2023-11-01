51469a9134fd2f855f1c36d285a89f31d49261e6
Vexl1 <51213834+Vexl1@users.noreply.github.com>
2023-11-01 14:07:38 -0400

    updated: app.py

--- app.py
+++ app.py
@@ -50,28 +50,6 @@ def location(loc: str):
     return jsonify(data=forecastData, 
                    location=forecastData['location'], current=forecastData['current'], condition=forecastData['current']['condition'],
                    forecast=forecastData['forecast']['forecastday'])
-def location(loc: str):
-    loc = request.args.get('inputValue')
-    loc = loc.split(" ")[0] 
-    if loc == "current":
-        latlng = get_location()
-       
-        response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={latlng[0]},{latlng[1]}&days=14")
-    else:
-       # get the input from text box html
-       input = request.args.get('inputValue')
-       input = input.replace(" ", "%20")
-      # print(input)
-       response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={input}&days=14")
-       
-    global forecastData
-    # Convert the response to JSON and store it in forecastData
-    forecastData = json.loads(response.read())
-    
-    # Convert data to JSON to use in main.js
-    return jsonify(data=forecastData, 
-                   location=forecastData['location'], current=forecastData['current'], condition=forecastData['current']['condition'],
-                   forecast=forecastData['forecast']['forecastday'])
 
 ######################
 # This function Get the card date from 
@@ -94,6 +72,7 @@ def getDate():
             
    
     return  jsonify(hourlyData)
+
 ################################
 # This function Checks if the 
 # Sun is up or Down 
@@ -141,79 +120,6 @@ def CheckSun():
 @app.route('/default')
 def default():
    response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={get_location()[0]},{get_location()[1]}&days=3")
-   global forecastData
-   forecastData  = json.loads(response.read())
-   return jsonify( location=forecastData['location'], current=forecastData['current'], condition=forecastData['current']['condition'],
-                   forecast=forecastData['forecast']['forecastday'])
-######################
-# This function Get the card date from 
-# Main.js and returns the hourly data
-# for that date
-#######################
-@app.route('/getDate', methods=['POST'])
-def getDate():
-    CheckSun()
-    cardDate = request.json 
-   # print(cardDate)
-    hourlyData = None
-    #print(forecastData)
-    for forecast_day in forecastData['forecast']['forecastday']:
-       
-        if forecast_day['date'] == cardDate:
-         #You can access the data for the specific day here
-             hourlyData = forecast_day['hour']
-             break
-            
-   
-    return  jsonify(hourlyData)
-################################
-# This function Checks if the 
-# Sun is up or Down 
-################################
-@app.route('/CheckSun', methods=['GET'])
-def CheckSun():
-    print ('Change Background')
-    sunrise_time_str = forecastData['forecast']['forecastday'][0]['astro']['sunrise']
-    sunset_time_str = forecastData['forecast']['forecastday'][0]['astro']['sunset']
-    local_time_str = forecastData['location']['localtime']
-    
-    local_time_str = local_time_str.split(' ', 1)[1]
-   
-    
-    print(local_time_str)
-    
-    # Parse the input time into a datetime object
-    time_12h = datetime.strptime(sunset_time_str, "%I:%M %p")
-
-    # Convert to 24-hour format
-    sunset_time_str = time_12h.strftime("%H:%M")
-    time_12h = datetime.strptime(sunrise_time_str, "%I:%M %p")
-    sunrise_time_str = time_12h.strftime("%H:%M")
-   
-
-    # Extract hours and minutes from the time
-    hours, minutes = map(int, local_time_str.split(':'))
-    sunset_time_str = sunset_time_str.split(':')
-    sunrise_time_str =  sunrise_time_str.split(':')
-    
-    daytime_start = (int(sunrise_time_str[0]), int(sunrise_time_str[1]))  
-    daytime_end = (int(sunset_time_str[0]),int(sunset_time_str[1])) 
-    # Compare the extracted time with the daytime and nighttime ranges
-    if (daytime_start <= (hours, minutes) <= daytime_end):
-        suntime = 'day'
-    else:
-        suntime = 'night'
-        
-    
-    return jsonify(sunrise=sunrise_time_str, sunset=sunset_time_str, suntime=suntime, localtime=local_time_str)
-
-################################
-# Get the default location which is the user location
-################################
-@app.route('/default')
-def default():
-   response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={get_location()[0]},{get_location()[1]}&days=3")
-   global forecastData
    forecastData  = json.loads(response.read())
    return jsonify( location=forecastData['location'], current=forecastData['current'], condition=forecastData['current']['condition'],
                    forecast=forecastData['forecast']['forecastday'])
@@ -234,16 +140,6 @@ def index():
         json_data = location(str)
         return json_data
    return render_template("index.html", data=forecastData,   location=forecastData['location'])
-   global forecastData
-   forecastData  = json.loads(response.read())
-   #print( forecastData)
-  
-   if request.is_json:
-       
-        json_data = location(str)
-        return json_data
-   return render_template("index.html", data=forecastData,   location=forecastData['location'])
-
 ################################
 # This Function Runs the server 
 # on a local machine 
