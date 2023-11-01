f20aa802f5f18049e230a8b3874e47f1449a79c4
allie090 <allie.marcum02@gmail.com>
2023-11-01 12:36:03 -0400

    Dropdown Functionality

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template, request, jsonify
 from urllib.request import urlopen
 import json
 import geocoder
+from datetime import datetime
 
 app = Flask(__name__)
 
+
+
 API_KEY = '5dea31b4204948a681b182600230709'
+
 
 ################################
 # This function Get the 
@@ -22,42 +26,121 @@ def get_location():
 # to weatherapi to get the weekly 
 # forecast
 ################################
-@app.route('/location/<loc>')
-def location(loc: str = "Paris"):
-    response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key=5dea31b4204948a681b182600230709&q={loc}&days=14")
-    data = json.loads(response.read())
+def location(loc: str):
+    loc = request.args.get('inputValue')
+    loc = loc.split(" ")[0] 
+    if loc == "current":
+        latlng = get_location()
+       
+        response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={latlng[0]},{latlng[1]}&days=14")
+    else:
+       # get the input from text box html
+       input = request.args.get('inputValue')
+       input = input.replace(" ", "%20")
+      # print(input)
+       response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={input}&days=14")
+       
+    global forecastData
+    # Convert the response to JSON and store it in forecastData
+    forecastData = json.loads(response.read())
+    
+    # Convert data to JSON to use in main.js
+    return jsonify(data=forecastData, 
+                   location=forecastData['location'], current=forecastData['current'], condition=forecastData['current']['condition'],
+                   forecast=forecastData['forecast']['forecastday'])
 
-    # Saves data to json for debugging purposes
-    with open(f'loc_json/loc_{loc}.json', 'w') as f:
-        json.dump(data, f, indent=4)
+######################
+# This function Get the card date from 
+# Main.js and returns the hourly data
+# for that date
+#######################
+@app.route('/getDate', methods=['POST'])
+def getDate():
+    CheckSun()
+    cardDate = request.json 
+   # print(cardDate)
+    hourlyData = None
+    #print(forecastData)
+    for forecast_day in forecastData['forecast']['forecastday']:
+       
+        if forecast_day['date'] == cardDate:
+         #You can access the data for the specific day here
+             hourlyData = forecast_day['hour']
+             break
+            
+   
+    return  jsonify(hourlyData)
+################################
+# This function Checks if the 
+# Sun is up or Down 
+################################
+@app.route('/CheckSun', methods=['GET'])
+def CheckSun():
+    print ('Change Background')
+    sunrise_time_str = forecastData['forecast']['forecastday'][0]['astro']['sunrise']
+    sunset_time_str = forecastData['forecast']['forecastday'][0]['astro']['sunset']
+    local_time_str = forecastData['location']['localtime']
+    
+    local_time_str = local_time_str.split(' ', 1)[1]
+   
+    
+    print(local_time_str)
+    
+    # Parse the input time into a datetime object
+    time_12h = datetime.strptime(sunset_time_str, "%I:%M %p")
 
-    return render_template("location.html", data=data, 
-                           location=data['location'], current=data['current'], condition=data['current']['condition'],
-                           forecast=data['forecast']['forecastday'])
+    # Convert to 24-hour format
+    sunset_time_str = time_12h.strftime("%H:%M")
+    time_12h = datetime.strptime(sunrise_time_str, "%I:%M %p")
+    sunrise_time_str = time_12h.strftime("%H:%M")
+   
 
-@app.route('/search')
-def search():
-    query = request.args.get('query', 'Paris')
-    query = query.replace(" ", "%20")
-    response = urlopen(f"https://api.weatherapi.com/v1/search.json?key={API_KEY}&q={query}")
-    data = json.loads(response.read())
+    # Extract hours and minutes from the time
+    hours, minutes = map(int, local_time_str.split(':'))
+    sunset_time_str = sunset_time_str.split(':')
+    sunrise_time_str =  sunrise_time_str.split(':')
+    
+    daytime_start = (int(sunrise_time_str[0]), int(sunrise_time_str[1]))  
+    daytime_end = (int(sunset_time_str[0]),int(sunset_time_str[1])) 
+    # Compare the extracted time with the daytime and nighttime ranges
+    if (daytime_start <= (hours, minutes) <= daytime_end):
+        suntime = 'day'
+    else:
+        suntime = 'night'
+        
+    
+    return jsonify(sunrise=sunrise_time_str, sunset=sunset_time_str, suntime=suntime, localtime=local_time_str)
 
-    # Saves data to json for debugging purposes
-    with open(f'loc_json/search_{query}.json', 'w') as f:
-        json.dump(data, f, indent=4)
-
-    return render_template("search.html", data=data)
+################################
+# Get the default location which is the user location
+################################
+@app.route('/default')
+def default():
+   response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={get_location()[0]},{get_location()[1]}&days=3")
+   global forecastData
+   forecastData  = json.loads(response.read())
+   return jsonify( location=forecastData['location'], current=forecastData['current'], condition=forecastData['current']['condition'],
+                   forecast=forecastData['forecast']['forecastday'])
 
 ################################
 # This functions renders the 
 # index webpage 
 ################################
 @app.route('/')
+@app.route('/')
+@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
    response = urlopen(f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={get_location()[0]},{get_location()[1]}&days=3")
-   data = json.loads(response.read())
-
-   return render_template("index.html", data=data)
+   global forecastData
+   forecastData  = json.loads(response.read())
+   #print( forecastData)
+  
+   if request.is_json:
+       
+        json_data = location(str)
+        return json_data
+   return render_template("index.html", data=forecastData,   location=forecastData['location'])
 
 ################################
 # This Function Runs the server 
--- static/Javascript/main.js
+++ static/Javascript/main.js
@@ -1,11 +1,148 @@
-// Wait until html is done rendering 
-$(document).ready(function() {
+
+// Get the current input value from the Location input field.
+var inputValue;
+
+//  Store the previous card data for later retrieval.
+var previouscard;
+
+// Store hourly temperature data.
+let hourlytemp = [];
+
+
+
+// Store timestamp data.
+let timestamp = [];
+function Autocorrect(){
+  var input = document.getElementById('Location');
+  var autocomplete = new google.maps.places.Autocomplete(input, { types: ['(cities)']['geocode'] });
+  google.maps.event.addListener(autocomplete, 'place_changed', function () {
+      var place = autocomplete.getPlace();
+  })
+}
+
+/**
+ * Get location information and update the UI.
+ * @param {Object} forecastWeek - The forecast data for the week.
+ */
+function getLocation(forecastWeek) {
+  inputValue = `${forecastWeek.location.name}, ${forecastWeek.location.region}, ${forecastWeek.location.country}`;
+  $('#Location').val(inputValue); 
+}
+/**
+ * Store the previous card data before switching to hourly data.
+ */
+function previousCard(){
+  previouscard = $('#Cards').html();
+}
+
+/**
+ * Set the Background of the webpage
+ */
+function SetBackground(){
+  $.ajax({
+    url:'/CheckSun',
+    type:'get',
+    contentType:'application/json',
+  
+    success: function (response) {
+      console.log(response.suntime)
+      console.log(response.suntime=== 'day')
+       if(response.suntime=== 'day'){
+        $('body').attr('class', 'sunny-bg')
+       } else {
+        $('body').attr('class', 'moon-bg');
+       }
+
+      $("#suntime").append(response.suntime);
+},
+error: function(error) {
+console.error('Error:', error);
+}    
+})
+}
+
+/**
+ * Get the Word Day of the week I.e (Sunday , Monday, etc)
+ * @param {String} dateString - The date string given by the python server
+ * @returns {String} - the Word day of the Week
+ */
+function getDayOfWeek(dateString) {
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const date = new Date(dateString + 'T00:00:00');
+  const dayOfWeek = date.getDay();
+  return daysOfWeek[dayOfWeek];
+}
+
+/*
+* Get the default weather Card
+*/
+function defaultCard(){
+  console.log('default card')
+  $.ajax({
+    url:'/default',
+    type:'get',
+    contentType:'application/json',
     
-$("#query").on('keyup', function (event) {
+    success: function (response) {
+        console.log(response)
+        
+const cardsContainer = $('#Cards');
+cardsContainer.empty(); // Clear existing card data
+const forecastWeek  = response
+getLocation(forecastWeek)
+// Loop through the forecast data and create cards
+for (const forecast of forecastWeek.forecast) {
+ 
+//console.log(forecast)
+// Append a new card to div Cards 
+   const cardHtml = `
+    <div class="col-md-2">
+      <div id="CardBlock">
+        <button class = "no-outline-button" value= ${forecast.date} Onclick = "ShowHourly(value)">
+        <div class="card">
+          <img class="card-img-top" src="${forecast.day.condition.icon}" alt="Card image">
+          <div class="card-img-overlay" style="text-align: center;">
+            <h4 class="card-title"><b>${getDayOfWeek(forecast.date) }</b></h4>
+            <p class="card-text"><h3>${forecast.day.condition.text}</h3></p>
+            <img src="static/Images/thermometer.png" alt="Card image" width="20" height="20">
+            Low ${Math.round(forecast.day.mintemp_f)}°F |  ${Math.round(forecast.day.avgtemp_f)}°F | High ${Math.round(forecast.day.maxtemp_f)}°F 
+          </div>
+        </div>
+        </button>
+      </div>
+    </div>
+  `;// Need to change Mesurement if the user has chosen another one
+  cardsContainer.append(cardHtml);
+}
+SetBackground()
+
+},
+error: function(error) {
+console.error('Error:', error);
+}    
+})
+
+}
+
+
+
+/**
+ * Initialize the page with data and event handlers.
+ */
+$(document).ready(function() {
+  Autocorrect()
+  previousCard()
+  KeyPress = false
+$("#Location").on('keyup', function (event) {
     if (event.keyCode === 13) {
+      KeyPress = true;
+      document.body.className = '';
         console.log("Enter key pressed!!!!!");
-        var inputValue = $(this).val();
+        inputValue = $(this).val();
+        
         console.log(inputValue);
+
+
         $.ajax({
             url:'',
             type:'get',
@@ -15,56 +152,386 @@ $("#query").on('keyup', function (event) {
             }, 
             success: function (response) {
                 console.log(response)
-              
+                
         const cardsContainer = $('#Cards');
         cardsContainer.empty(); // Clear existing card data
         const forecastWeek  = response
-
+        getLocation(forecastWeek)
         // Loop through the forecast data and create cards
         for (const forecast of forecastWeek.forecast) {
-        //date string in "YYYY-MM-DD" format
-        var dateString = forecast.date;
-
-        // Parse the date string into a Date object
-        var date = new Date(dateString);
-
-        // Array of day names
-        var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-        // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
-        var dayOfWeek = date.getDay();
-
-        // Get the name of the day
-        var dayName = dayNames[dayOfWeek];
-        console.log(forecast)
+        //console.log(forecast)
         // Append a new card to div Cards 
            const cardHtml = `
             <div class="col-md-2">
               <div id="CardBlock">
-              <button type="button" class="no-outline-button">
+                <button class = "no-outline-button" value= ${forecast.date} Onclick = "ShowHourly(value)">
                 <div class="card">
                   <img class="card-img-top" src="${forecast.day.condition.icon}" alt="Card image">
                   <div class="card-img-overlay" style="text-align: center;">
-                    <h4 class="card-title"><b>${dayName}</b></h4>
+                    <h4 class="card-title"><b>${getDayOfWeek(forecast.date)}</b></h4>
                     <p class="card-text"><h3>${forecast.day.condition.text}</h3></p>
                     <img src="static/Images/thermometer.png" alt="Card image" width="20" height="20">
-                     Low ${forecast.day.mintemp_f } °F | Avg ${forecast.day.avgtemp_f}°F | High ${forecast.day.maxtemp_f}°F 
+                    Low ${Math.round(forecast.day.mintemp_f)}°F |  ${Math.round(forecast.day.avgtemp_f)}°F | High ${Math.round(forecast.day.maxtemp_f)}°F 
                   </div>
                 </div>
                 </button>
               </div>
             </div>
-          `;
-
+          `;// Need to change Mesurement if the user has chosen another one
+         
           cardsContainer.append(cardHtml);
         }
+        SetBackground()
       },
       error: function(error) {
         console.error('Error:', error);
       }    
         })
-     }
+     }  
  })
-
+ if(!KeyPress){
+  console.log("Not Press")
+  defaultCard()
+ } 
 })
 
+
+
+/**
+ * Convert time from 24:00 to 12:00 format.
+ * @param {String} DateTime - Time in 24-hour format.
+ * @returns {String} - Time in 12-hour format.
+ */
+function GetTime(DateTime) {
+const parsedDate = new Date(DateTime);
+
+// Get the time portion (hours and minutes)
+const hours = parsedDate.getHours();
+const minutes = parsedDate.getMinutes();
+
+// Convert to 12-hour format
+const period = hours >= 12 ? "PM" : "AM";
+const formattedTime = `${hours % 12 || 12}:${minutes.toString().padStart(2, '0')} ${period}`;
+return formattedTime
+}
+
+function changeTempUnit() { 
+  const temperatureUnit = document.getElementById("metricDropdown").value;
+
+  updateTempUnit(temperatureUnit); 
+}
+
+
+//FIX
+function updateTempUnit(temperatureUnit) {
+
+  // Check the selected temperature unit and update the temperature values
+  if (temperatureUnit === "METRIC | C") {
+      // Convert temperature values to Celsius
+      for (let i = 0; i < hourly_Data.length; i++) {
+          hourly_Data[i].temp_f = (hourly_Data[i].temp_f - 32) * 5/9;
+          hourly_Data[i].feelslike_f = (hourly_Data[i].feelslike_f - 32) * 5/9;
+      }
+  } else {
+      // Convert temperature values back to Fahrenheit
+      for (let i = 0; i < hourly_Data.length; i++) {
+          hourly_Data[i].temp_f = hourlytemp[i];
+          hourly_Data[i].feelslike_f = hourly_Data[i].feelslike_f;
+      }
+  }
+
+  for (let i = 0; i < hourly_Data.length; i++) {
+      cardHtml = `
+      <br><div class="col-md-2">
+      <h4>${formattedDateTime}</h4>
+      <img src="${hourly_Data[i].condition.icon}" alt="Card image">
+      <h5>${hourly_Data[i].condition.text}</h5>
+      <br> ${Math.round(hourly_Data[i].temp_f)} ${temperatureUnit === "METHRIC | C" ? "°C" : "°F"} Feels Like ${Math.round(hourly_Data[i].feelslike_f)} ${temperatureUnit === "METRIC | C" ? "°C" : "°F"} </br>
+      </div>
+      </div><br>
+      `;
+
+      $('#scroll').append(cardHtml);
+  }
+
+} 
+/**
+ * Renders a chart using ZingChart
+ * with the hourly date from hourlytemp
+ * and time from timstamp
+ */
+function createChart(){
+  console.log('createChart');
+  var myConfig = {
+    "globals": {
+      "font-family": "Roboto"
+    },
+    "graphset": [{
+      "type": "area",
+      "background-color": "#fff",
+      "utc": true,
+      "title": {
+        "y": "15px",
+        "text": "Hourly Temperature Change",
+        "background-color": "none",
+        "font-color": "#05636c",
+        "font-size": "24px",
+        "height": "25px",
+        "adjust-layout": true
+      },
+      "plotarea": {
+        "margin-top": "10%",
+        "margin-right": "dynamic",
+        "margin-bottom": "dynamic",
+        "margin-left": "dynamic",
+        "adjust-layout": true
+      },
+      "labels": [{
+          "text": "",
+          "default-value": "",
+          "color": "#8da0cb",
+          "x": "20%",
+          "y": 50,
+          "width": 120,
+          "text-align": "left",
+          "bold": 0,
+          "font-size": "14px",
+          "font-weight": "bold"
+        },
+        
+      ],
+      "scale-x": {
+        "label": {
+          "text": "Time",
+          "font-size": "14px",
+          "font-weight": "normal",
+          "offset-x": "10%",
+          "font-angle": 360
+        },
+        "item": {
+          "text-align": "center",
+          "font-color": "#05636c"
+        },
+        "zooming": 1,
+        "max-labels": 12,
+        "labels": [
+          timestamp[0],
+          timestamp[1],
+          timestamp[2],
+          timestamp[3],
+          timestamp[4],
+          timestamp[5],
+          timestamp[6],
+          timestamp[7],
+          timestamp[8],
+          timestamp[9],
+          timestamp[10],
+          timestamp[11],
+          timestamp[12],
+          timestamp[13],
+          timestamp[14],
+          timestamp[15],
+          timestamp[16],
+          timestamp[17],
+          timestamp[18],
+          timestamp[19],
+          timestamp[20],
+          timestamp[21],
+          timestamp[22],
+          timestamp[23]
+        ],
+        "max-items": 12,
+        "items-overlap": true,
+        "guide": {
+          "line-width": "0px"
+        },
+        "tick": {
+          "line-width": "2px"
+        },
+      },
+      "crosshair-x": {
+        "line-color": "#fff",
+        "line-width": 1,
+        "plot-label": {
+          "visible": false
+        }
+      },
+      "scale-y": {
+        "values": "0:120:50",
+        "item": {
+          "font-color": "#05636c",
+          "font-weight": "normal"
+        },
+        "label": {
+          "text": "Temperature",
+          "font-size": "14px"
+        },
+        "guide": {
+          "line-width": "0px",
+          "alpha": 0.2,
+          "line-style": "dashed"
+        }
+      },
+      "plot": {
+        "line-width": 2,
+        "marker": {
+          "size": 1,
+          "visible": false
+        },
+        "tooltip": {
+          "font-family": "Roboto",
+          "font-size": "15px",
+          "text": "It feels like %v F",
+          "text-align": "left",
+          "border-radius": 5,
+          "padding": 10
+        }
+      },
+      "series": [{
+          "values": [
+            hourlytemp[0],
+            hourlytemp[1],
+            hourlytemp[2],
+            hourlytemp[3],
+            hourlytemp[4],
+            hourlytemp[5],
+            hourlytemp[6],
+            hourlytemp[7],
+            hourlytemp[8],
+            hourlytemp[9],
+            hourlytemp[10],
+            hourlytemp[11],
+            hourlytemp[12],
+            hourlytemp[13],
+            hourlytemp[14],
+            hourlytemp[15],
+            hourlytemp[16],
+            hourlytemp[17],
+            hourlytemp[18],
+            hourlytemp[19],
+            hourlytemp[20],
+            hourlytemp[21],
+            hourlytemp[22],
+            hourlytemp[23],
+           
+          ],
+          "Time": [
+            timestamp[0],
+            timestamp[1],
+            timestamp[2],
+            timestamp[3],
+            timestamp[4],
+            timestamp[5],
+            timestamp[6],
+            timestamp[7],
+            timestamp[8],
+            timestamp[9],
+            timestamp[10],
+            timestamp[11],
+            timestamp[12],
+            timestamp[13],
+            timestamp[14],
+            timestamp[15],
+            timestamp[16],
+            timestamp[17],
+            timestamp[18],
+            timestamp[19],
+            timestamp[20],
+            timestamp[21],
+            timestamp[22],
+            timestamp[23],
+          ],
+          "line-color": "#fc8d62",
+          "aspect": "spline",
+          "background-color": "#fc8d62",
+          "alpha-area": ".5",
+          "font-family": "Roboto",
+          "font-size": "14px",
+          "text": "returns"
+        }
+      ]
+    }]
+  };
+
+  zingchart.render({
+    id: 'chart',
+    data: myConfig,
+    height: '100%',
+    width: '100%'
+  });
+}
+
+
+  
+
+/**
+ * Show hourly data for a specific day.
+ * @param {String} day - The selected day for hourly data.
+ */
+function ShowHourly(day){
+  console.log(day)
+  
+  $.ajax({
+    url: '/getDate',
+    method: 'POST',
+    data: JSON.stringify( day ), 
+    contentType: 'application/json',
+    success: function(response) {
+      previouscard = $('#Cards').html();
+      // Parse the JSON response
+      var hourly_Data = response;
+      const cardsContainer = $('#Cards');
+      cardsContainer.empty(); // Clear existing card data
+
+      backButton= `<button class="button-19" role="button" Onclick = "Return()"> Back </button><!-- HTML !-->
+     `;
+      cardsContainer.append(backButton)
+
+      const scrollHtml = `
+      <div class="container horizontal-scrollable"> 
+      <div id = "scroll" class="row text-center"style="height: 40vh;"> 
+      </div> 
+      </div>
+      <div id="chart" style="height: 40vh;"></div> 
+      `;
+      cardsContainer.append(scrollHtml);
+      for ( i = 0; i < hourly_Data.length; i++) {
+        // Get the formatted time string
+       
+        const formattedDateTime = GetTime(hourly_Data[i].time);
+        hourlytemp.push(hourly_Data[i].temp_f);
+        timestamp.push(formattedDateTime)
+      
+         cardHtml =  `
+        <br><div class="col-md-2">
+        <h4>${formattedDateTime}</h4>
+        <img src="${hourly_Data[i].condition.icon}" alt="Card image">
+        <h5>${hourly_Data[i].condition.text}</h5>
+        <br> ${Math.round(hourly_Data[i].temp_f)} °F  Feels Like ${Math.round(hourly_Data[i].feelslike_f)} °F </br>
+        </div>
+        </div><br>
+      `;// Need to change Mesurement if the user has chosen another one
+       
+      $('#scroll').append(cardHtml)
+       // console.log("DATA : "+ hourly_Data[i].feelslike_f)
+      }
+      createChart();
+      console.log(hourly_Data);
+    },
+    error: function(error) {
+      console.error('Error:', error);
+    }
+  });
+ 
+}
+
+/**
+ * Returns the previous Card ui
+ */
+function Return(){
+  const cardsContainer = $('#Cards');
+  cardsContainer.empty();
+  console.log('Returning');
+  cardsContainer.append(previouscard);
+  previouscard = null;
+}
